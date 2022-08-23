b8759509bea5c138017682d2a3b09bd0d68f6b20
kirineko <kirineko@qq.com>
2022-08-24 01:30:29 +0800

    day1

---
+++ w1/MetaCoin/contracts/Counter.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint256 public counter;
+
+    constructor() {
+        counter = 0;
+    }
+
+    function count() public {
+        counter = counter + 1;
+    }
+}
